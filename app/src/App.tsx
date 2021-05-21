b831ab1d4187ea8f44bbec975ed8f6223dbd0f32
Nicola Sacco <nicola.sacco@volkswagen.de>
2021-05-21 10:15:27 +0200

    Added the fetch of some pets from the api

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [pets, setPets]=useState<any[]|null>(null);
+  const [loading,setLoading]=useState<boolean>(true);
+
+  useEffect(()=>{
+    const url='http://localhost:8000/pets'
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          setPets(data.pets);
+          setLoading(false);
+        });
+  },[setPets,setLoading])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +31,16 @@ function App() {
         >
           Learn React
         </a>
+        {(!loading && pets) && <div>
+          <p>
+            Here, take some pets
+          </p>
+          <ul>
+          {pets.map(pet=><li>
+            {pet.name}: {pet.age} years old
+          </li>)}
+          </ul>
+        </div>}
       </header>
     </div>
   );
